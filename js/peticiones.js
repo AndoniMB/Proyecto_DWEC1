6254dcd5045eba3119c4d06c3004e0a769e2637e
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2025-11-25 09:51:01 +0100

    añadidas todas las peticiones vacias

---
+++ js/peticiones.js
@@ -0,0 +1,27 @@
+//get de los equipos
+export async function getEquipos() {
+    
+}
+
+//get de los jugadores
+export async function getJugadores() {
+    
+}
+//post de los jugadores
+export async function postJugadore() {
+    
+}
+
+//put de los jugadores
+export async function putJugadore() {
+    
+}
+//patch de los jugadores
+export async function patchJugadore() {
+    
+}
+
+//delete de los jugadores
+export async function deleteJugadore() {
+    
+}
