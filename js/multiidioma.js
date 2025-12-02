74e9e13190ab3b634cb24e6217275de8494745ad
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2025-12-02 07:57:19 +0100

    corregido el comentario

--- js/multiidioma.js
+++ js/multiidioma.js
@@ -1,7 +1,7 @@
 let strings = {};  // aquí se guardarán los textos cargados
 
 export async function loadLanguage(lang) {
-    //meter el idioma en la sesion, esto deberia hacerse en otro archivo
+    //meter el idioma en la sesion
     sessionStorage.setItem("idioma",lang);
 
     // carga el idioma seleccionado
